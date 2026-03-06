99d4ff0cd16f0ca84195d618d3358cd1fe4fadec
tueej <tuetdm@gmail.com>
2026-03-06 11:37:24 +0700

    thêm database postgresql, lỗi: Module prisma/client has no exported member Client

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '../../prisma/node_modules/.prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService
